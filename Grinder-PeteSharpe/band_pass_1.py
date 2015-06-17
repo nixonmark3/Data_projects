5503efa209f0d40c6ce79e012fc3940f4fbdebff
noelbell <noel.bell@emerson.com>
2015-06-17 16:55:14 -0500

    code for investigating Pete Sharpe's SAG (semi-autonomous grinder) data

--- Grinder-PeteSharpe/band_pass_1.py
+++ Grinder-PeteSharpe/band_pass_1.py
@@ -3,73 +3,45 @@ __author__ = 'Alper'
 import pandas as pd
 
 import scipy as sp
+import scipy.signal as signal
 import numpy as np
 
 
-def summary_fft(df, tag, window_num, start_index, end_index, window_size, no_of_freq, df_fft):
-    print('start and end.......', df.iloc[start_index,:], df.iloc[end_index,:])
-    data = df[tag].ix[start_index:end_index]
-    # sp.fft returns fft with frequencies and magnitudes
-    df_fft2 = df_fft
-    fft_data = sp.fft(data)
-
-    print('shape = ', np.shape(fft_data))
-    #print(fft_data)
-    result = []
-    abs_fft_data = np.abs(fft_data)
-    #df_fft_data = pd.DataFrame(abs_fft_data[1:])
-    #while len(result) < no_of_freq:
-
-
-        # freq returned from sp.fft is cycles/window but we want uniform cycles per day.  1440 could be parameterized
-        # different from 1440
-        #freq = freq * 3840 / window_size
-    result.append((fft_data))
-        #print(freq, max_value)
-    # adding frequencies and magnitudes to dataframe
-    #print('result.....\n', result)
-    for i, x in enumerate(result):
-        df_fft2['freq{0}'.format(i + 1)].ix[window_num] = result[i][0]
-        df_fft2['max_val{0}'.format(i + 1)].ix[window_num] = result[i][1]
-    #print('df_fft2.......\n', df_fft2)
-    return df_fft2, fft_data
-
-
-def rolling_fft(df, tag, no_of_freq, window_size):
-    # add columns for freq and magnitude results
-    df_fft = pd.DataFrame()
-    df_fft_all = pd.DataFrame()
-    for i, x in enumerate(range(no_of_freq)):
-        df_fft['freq{0}'.format(i + 1)] = [np.NaN] * 1152
-        df_fft['max_val{0}'.format(i + 1)] = [np.NaN] * 1152
-
-    #for i in range((len(df.index) - window_size)//3840):
-    #for i in range(1152):
-    for i in range(0,20):
-        j = i * 1024
-        #print(i, j, range((len(df.index) - window_size)//3840))
-        df_fft2, fft_data = summary_fft(df, tag,i, j, j + window_size, window_size, no_of_freq,df_fft)
-        df_fft_all.loc[:,'freq{0}'.format(i)] = fft_data
-        
-    return df_fft2, df_fft_all
-
-
 def main():
-     df = pd.read_csv('/Users/noelbell/bds_datafiles/CuMtn_Vib_Data_Apr_2-6_2015/df_all_Node33286b.csv', parse_dates=True, index_col=0)
+     df = pd.read_csv('/Users/noelbell/bds_datafiles/CuMtn_Vib_Data_Apr_2-6_2015/df_ch3_b.csv', parse_dates=True, index_col=0)
     #df = py_wrap.load_data_set()
+     print('df......\n', df)
+#     x_input = df['ch3'].values
+     x_input = df.values
 
-     no_of_freq = 110
+     filt_order = 6
+     f_type = 'bandpass'
+     analog_or_digital = False
+     output_type = 'ba'
+     wn_band = [.25, .75]
 
-     window_size = 2048
-     # tag hardwired....any and all tags in dataset
-     print('calling rolling.... \n ...... \n .......  \n...... \n .......')
-     df_fft2, df_fft_data = rolling_fft(df, 'ch3', no_of_freq, window_size)
+# get the b and a parameters of the filter:
+     b, a = signal.butter(filt_order, wn_band, f_type, analog_or_digital, output_type )
+     print('b.......', b)
+     print('a.......', a)
+     print('x input', x_input.T)
 
-     print(df_fft_data)
+# filter the data
+     x_filt = signal.lfilter(b,a, x_input.T)
+     print('xfilt type.....', type(x_filt))
+     print('x_filt....', x_filt)
+     print('x_filt.bracket...', x_filt[1:,1:])
+     df_new = pd.DataFrame(x_filt[1:,1:])
+#     df_new.loc[:,'filt'] = x_filt.T
+ #    print(' size of filt....', df.shape)
 
-
-     name='/Users/noelbell/bds_datafiles/CuMtn_Vib_Data_Apr_2-6_2015/fft_b_nomax.csv'
-     df_fft_data.to_csv(name)
+     for i in range(0,1):
+        j = i * 50000
+        j2 = j + 50000
+        name='/Users/noelbell/bds_datafiles/CuMtn_Vib_Data_Apr_2-6_2015/fft_b_ch3_filt_{0}.csv'.format(i)
+        short_df = df_new.iloc[:,j:j2]
+        print(short_df)
+        short_df.to_csv(name)
 
 
 if __name__ == "__main__":
